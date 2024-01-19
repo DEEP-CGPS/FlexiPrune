128736f3d0eeb7718354b9825309087558d72bb5
DEEP-CGPS <u3900259@unimilitar.edu.co>
2024-01-18 21:48:05 -0500

    update SETUP.py information

--- setup.py
+++ setup.py
@@ -17,18 +17,16 @@ setup(
     version=VERSION,
     author="DEEP-CGPS",
     author_email="<est.cesar.pachon@unimilitar.edu.co>",
+    url='https://github.com/DEEP-CGPS/PruningDistribution',
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'torch',
+        'torchvision',
+    ],
     keywords=['python', 'pytorch', 'pruning', 'CNN', 'distribution', 'FLOPs'],
-    classifiers=[
-        "Development Status :: 1 - Production",
-        "Intended Audience :: Reasearch",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    python_requires='>=3.6'
+
 )
